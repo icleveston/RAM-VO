90f116d5c9d07b28abb4a4929540a3ebfff03f1a
Iury Cleveston <ra230216@students.ic.unicamp.br>
2020-03-22 20:03:44 -0300

    initial files

--- src/main.py
+++ src/main.py
@@ -1,2 +1,47 @@
+import os
+import sys
+import cv2
+import numpy as np
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
+
+from datasets import Kitti, Tum, Euroc, Zurich, Cityscapes, Make3D
+
+
+def main():
+
+    # Create the kitti dataset
+    #k = Kitti(sequence="01")
+    #k = Tum(sequence="sequence_01")
+    #k = Euroc(sequence='MH_01_easy')
+    #k = Zurich()
+    k = Cityscapes(city='aachen', type='train')
+
+    for frame in k.next_frame():
+        cv2.imshow("frame", frame)
+        cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+def plot_make3D():
+
+    k = Make3D(dataset='dataset_1', type='test')
+
+    for image, depth in zip(k.next_image(), k.next_depth()):
+
+        image = cv2.resize(image, (460, 345))
+
+        cv2.imshow("image", image)
+
+        d = np.asarray(depth['Position3DGrid'])
+
+        d_resize = cv2.resize(d, (460, 345))
+
+        cv2.imshow("depth", d_resize)
+        cv2.waitKey(0)
+
+if __name__ == '__main__':
+    #main()
+    plot_make3D()
 
 
