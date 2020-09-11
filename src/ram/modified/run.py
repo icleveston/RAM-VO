39e5c89ec2662d5e3c3d179b1c3380fbbe0c9a8b
Iury Cleveston <ra230216@students.ic.unicamp.br>
2020-09-11 09:44:50 -0300

    Functional mnist ram

--- src/ram/modified/run.py
+++ src/ram/modified/run.py
@@ -1,10 +1,11 @@
 import argparse
-from dataset import MNIST 
+from pixel_dataset import Pixel 
+from mnist_dataset import MNIST 
 from ram import RAM
 
 class mnist_config():
     name = 'mnist'
-    output_path = 'out/'
+    output_path = 'out/mnist/'
     n_step = 6
     sample = 1
     glimpse_num = 8
@@ -16,7 +17,42 @@ class mnist_config():
     im_size = 28
     lr = 1e-3
     im_channel=1
+    dim_out=2
     n_class=10
+    max_grad_norm=5.0
+    
+class pixel_config():
+    name = 'pixel'
+    output_path = 'out/pixel/'
+    n_step = 6
+    sample = 1
+    glimpse_num = 8
+    glimpse_scale = 1
+    batch = 128
+    epoch = 1000
+    loc_std = 0.03
+    unit_pixel = 12
+    im_size = 28
+    lr = 1e-3
+    im_channel=3
+    dim_out=1
+    max_grad_norm=5.0
+
+class ball_config():
+    name = 'ball'
+    output_path = 'out/ball/'
+    n_step = 6
+    sample = 1
+    glimpse_num = 8
+    glimpse_scale = 1
+    batch = 128
+    epoch = 1000
+    loc_std = 0.03
+    unit_pixel = 12
+    im_size = 28
+    lr = 1e-3
+    im_channel=1
+    dim_out=2
     max_grad_norm=5.0
 
 def get_args():
@@ -24,7 +60,7 @@ def get_args():
     parser.add_argument('--predict', action='store_true', help='Run prediction')
     parser.add_argument('--train', action='store_true', help='Train the model')
     parser.add_argument('--eval', action='store_true', help='Evaluate the model')
-    parser.add_argument('--dataset', type=str, default='mnist', help='Use the mnist')
+    parser.add_argument('--dataset', type=str, default='pixel', help='Use the pixel dataset')
     parser.add_argument('--step', type=int, default=1, help='Number of glimpse')
     parser.add_argument('--sample', type=int, default=1, help='Number of location samples during training')
     parser.add_argument('--glimpse', type=int, default=12, help='Glimpse base size')
@@ -50,30 +86,30 @@ def main():
         
         valid_data = MNIST('val', batch_size=config.batch, shuffle=True, batch_dict_name=['data', 'label'])
     
-    elif FLAGS.dataset == 'regression_ball':
-        pass
+    elif FLAGS.dataset == 'pixel':
+        
+        config = pixel_config()
 
+        train_data = Pixel('train', batch_size=config.batch)
+        
+        valid_data = Pixel('val', batch_size=config.batch)
+
+    elif FLAGS.dataset == 'ball':
+        
+        config = ball_config()
+        
+        train_data = Ball('train', batch_size=config.batch)
+        
+        valid_data = Ball('val', batch_size=config.batch)
+    
     # Create the model
     model = RAM(config)
     
     if FLAGS.train:
-        
-        model.create_train_model()
-
-        # Train the model
         model.train(train_data, valid_data)
-        
     elif FLAGS.predict:
-        
-        model.create_predict_model()
-
         model.predict(FLAGS.load)
-    
     elif FLAGS.eval:
-        
-        model.create_predict_model()
-
-        # Evaluate the model
         model.evaluate(FLAGS.load)
     
 
---
+++ src/ram/oficial/ram_conan/lib/helper/trainer.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: trainer.py
+# Author: Qian Ge <geqian1001@gmail.com>
+
+import os
+import scipy.misc
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+
+class Trainer(object):
+    def __init__(self, model, train_data, init_lr=1e-3):
+        self._model = model
+        self._train_data = train_data
+        self._lr = init_lr
+
+        self._train_op = model.get_train_op()
+        self._loss_op = model.get_loss()
+        self._accuracy_op = model.get_accuracy()
+        self._sample_loc_op = model.layers['loc_sample']
+        self._pred_op = model.layers['pred']
+        self._lr_op = model.cur_lr
+
+        self.global_step = 0
+
+    def train_epoch(self, sess, summary_writer=None):
+        self._model.set_is_training(True)
+        cur_epoch = self._train_data.epochs_completed
+        step = 0
+        loss_sum = 0
+        acc_sum = 0
+        while cur_epoch == self._train_data.epochs_completed:
+            self.global_step += 1
+            step += 1
+
+            batch_data = self._train_data.next_batch_dict()
+            im = batch_data['data']
+            label = batch_data['label']
+            _, loss, acc, cur_lr = sess.run(
+                [self._train_op, self._loss_op, self._accuracy_op, self._lr_op], 
+                feed_dict={self._model.image: im,
+                           self._model.label: label,
+                           self._model.lr: self._lr})
+
+            loss_sum += loss
+            acc_sum += acc
+
+            if step % 100 == 0:
+                print('step: {}, loss: {:.4f}, accuracy: {:.4f}'
+                      .format(self.global_step,
+                              loss_sum * 1.0 / step,
+                              acc_sum * 1.0 / step))
+
+        print('epoch: {}, loss: {:.4f}, accuracy: {:.4f}, lr:{}'
+              .format(cur_epoch,
+                      loss_sum * 1.0 / step,
+                      acc_sum * 1.0 / step, cur_lr))
+        if summary_writer is not None:
+            s = tf.Summary()
+            s.value.add(tag='train/loss', simple_value=loss_sum * 1.0 / step)
+            s.value.add(tag='train/accuracy', simple_value=acc_sum * 1.0 / step)
+            summary_writer.add_summary(s, self.global_step)
+
+    def valid_epoch(self, sess, dataflow, batch_size, summary_writer=None):
+        self._model.set_is_training(False)
+        dataflow.setup(epoch_val=0, batch_size=batch_size)
+
+        step = 0
+        loss_sum = 0
+        acc_sum = 0
+        while dataflow.epochs_completed == 0:
+            step += 1
+            batch_data = dataflow.next_batch_dict()
+            loss, acc = sess.run(
+                [self._loss_op, self._accuracy_op], 
+                feed_dict={self._model.image: batch_data['data'],
+                           self._model.label: batch_data['label'],
+                           })
+            loss_sum += loss
+            acc_sum += acc
+        print('valid loss: {:.4f}, accuracy: {:.4f}'
+              .format(loss_sum * 1.0 / step, acc_sum * 1.0 / step))
+
+        if summary_writer is not None:
+            s = tf.Summary()
+            s.value.add(tag='valid/loss', simple_value=loss_sum * 1.0 / step)
+            s.value.add(tag='valid/accuracy', simple_value=acc_sum * 1.0 / step)
+            summary_writer.add_summary(s, self.global_step)
+
+        self._model.set_is_training(True)
