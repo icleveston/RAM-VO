aaa1e44087b2ff8ac94dbfde7d34d058684e2bab
Iury Cleveston <ra230216@students.ic.unicamp.br>
2020-10-16 09:11:07 -0300

    Ram regression

--- dataset_pixel.py
+++ dataset_pixel.py
@@ -15,7 +15,61 @@ motion_options = [
     (0, 1)
 ]
 
+class PixelContinuous100Dataset(Dataset):
 
+    def __init__(self, trans):
+           
+        self.name = "PixelContinuous100"
+        self.trans = trans
+           
+        data_dir = 'dataset/pixel_continuous_100/00/'
+        
+        self.path_frames = os.path.join(data_dir, 'images')
+        self.ground_truth = os.path.join(data_dir, "ground-truth.txt")
+        
+        self.content = []
+        
+        # Read the ground-truth lines
+        with open(self.ground_truth) as f:
+            self.content = f.readlines()
+        
+        # Remove the \n
+        self.content = [x.strip() for x in self.content] 
+        
+        # Count the groundtruth lines
+        self.length = len(self.content)-2
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        # Open image
+        image_t = cv2.imread(os.path.join(self.path_frames, f"{idx}.jpg"))
+        image_t_1 = cv2.imread(os.path.join(self.path_frames, f"{idx+1}.jpg"))
+        
+        # Concat the two images
+        images = [self.trans(image_t), self.trans(image_t_1)]
+        
+        # Read the ground truth
+        coordinates_t = self.content[idx].split(',')
+        coordinates_t_1 = self.content[idx+1].split(',')
+        
+        # Convert the coordinates to int
+        x_t = torch.tensor(float(coordinates_t[0]), dtype=torch.float)
+        y_t = torch.tensor(float(coordinates_t[1]), dtype=torch.float)
+        x_t_1 = torch.tensor(float(coordinates_t_1[0]), dtype=torch.float)
+        y_t_1 = torch.tensor(float(coordinates_t_1[1]), dtype=torch.float)
+        
+        # Compute the displacement
+        distance = torch.stack([x_t_1-x_t, y_t_1-y_t])
+            
+        return images, distance
+    
+    
 class PixelSkipped100Dataset(Dataset):
 
     def __init__(self, trans):
@@ -70,61 +124,6 @@ class PixelSkipped100Dataset(Dataset):
             
         return images, distance
     
-    
-class PixelSkipped25Dataset(Dataset):
-
-    def __init__(self, trans):
-           
-        self.name = "PixelSkipped25"
-        self.trans = trans
-           
-        data_dir = 'dataset/pixel_skipped_25/00/'
-        
-        self.path_frames = os.path.join(data_dir, 'images')
-        self.ground_truth = os.path.join(data_dir, "ground-truth.txt")
-        
-        self.content = []
-        
-        # Read the ground-truth lines
-        with open(self.ground_truth) as f:
-            self.content = f.readlines()
-        
-        # Remove the \n
-        self.content = [x.strip() for x in self.content] 
-        
-        # Count the groundtruth lines
-        self.length = len(self.content)-2
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, idx):
-        
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-
-        # Open image
-        image_t = cv2.imread(os.path.join(self.path_frames, f"{idx}.jpg"))
-        image_t_1 = cv2.imread(os.path.join(self.path_frames, f"{idx+1}.jpg"))
-        
-        # Concat the two images
-        images = [self.trans(image_t), self.trans(image_t_1)]
-        
-        # Read the ground truth
-        coordinates_t = self.content[idx].split(',')
-        coordinates_t_1 = self.content[idx+1].split(',')
-        
-        # Convert the coordinates to int
-        x_t = torch.tensor(float(coordinates_t[0]), dtype=torch.float)
-        y_t = torch.tensor(float(coordinates_t[1]), dtype=torch.float)
-        x_t_1 = torch.tensor(float(coordinates_t_1[0]), dtype=torch.float)
-        y_t_1 = torch.tensor(float(coordinates_t_1[1]), dtype=torch.float)
-        
-        # Compute the displacement
-        distance = torch.stack([x_t_1-x_t, y_t_1-y_t])
-            
-        return images, distance
-
 
 class PixelUniformDataset(Dataset):
 
